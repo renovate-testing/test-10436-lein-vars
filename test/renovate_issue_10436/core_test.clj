8f6111d94677bd5a207a51d315223b7d4d15b0e4
yevgeni.t <yevgeni.t@appsflyer.com>
2021-06-15 17:29:38 +0300

    Remove tests

--- test/renovate_issue_10436/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns renovate-issue-10436.core-test
-  (:require [clojure.test :refer :all]
-            [renovate-issue-10436.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
